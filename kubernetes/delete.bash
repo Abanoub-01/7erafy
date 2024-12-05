e28c10a5237c457b962085a5f0a717bce358a9f4
Abanoub-01 <nobamaroo2013@gmail.com>
2024-12-05 15:32:53 +0200

    Changes to be committed:

--- kubernetes/delete.bash
+++ kubernetes/delete.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-kubectl delete -f MongoDB.yaml
-kubectl delete -f MongoDB-pvc.yaml
-kubectl delete -f frontend.yaml 
+kubectl delete -f mongo.yaml
+kubectl delete -f backend.yaml
+kubectl delete -f frontend.yaml
 kubectl delete -f mongo-express.yaml
-kubectl delete -f frontend-svc.yaml 
-kubectl delete -f backend.yaml 
-kubectl delete -f backend-svc.yaml 
 
 minikube stop
---
+++ kubernetes/expose.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+minikube service my-app-front
+minikube service mongo-express
+minikube service my-app-back
